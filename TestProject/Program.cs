9dfe648557994382eb2a6c20c94bf38c590890ba
mat-123 <156803363+Mat-123@users.noreply.github.com>
2024-11-21 16:46:28 +0100

    Sfida dei metodi della classe Math

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,5 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Random dice = new Random();
-//int roll = dice.Next(1,7);
-//Console.WriteLine(roll);
-int result = dice.Next();
+int firstValue = 500;
+int secondValue = 600;
+int largerValue = Math.Max(firstValue, secondValue);
+
+
+Console.WriteLine(largerValue);
