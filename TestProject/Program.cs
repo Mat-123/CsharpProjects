89008e02602c0c3e4f40668bfe01cd118792c7fc
mat-123 <156803363+Mat-123@users.noreply.github.com>
2024-11-21 18:10:17 +0100

    Esercizio - Completare un'attività di sfida inerente all'applicazione di regole aziendali

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,44 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Random dice = new Random();
+Random random = new Random();
+int daysUntilExpiration = random.Next(12);
+int discountPercentage = 0;
 
-// int roll1 = dice.Next(1, 7);
-// int roll2 = dice.Next(1, 7);
-// int roll3 = dice.Next(1, 7);
-int roll1 = 6;
-int roll2 = 6;
-int roll3 = 6;
-
-int total = roll1 + roll2 + roll3;
-
-Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
-
-if((roll1 == roll2) || (roll1 == roll3) || (roll2 == roll3))
+if (daysUntilExpiration == 0)
 {
-    if((roll1 == roll2) && (roll2 == roll3))
-    {
-    Console.WriteLine("You rolled triples! +6 bonus to total.");
-    total += 6;
-    }
-    else
-    {
-    Console.WriteLine("You are really lucky rolling doubles, you get a +2 bonus!");
-    total += 2;
-    }
+    Console.WriteLine("Your subscription has expired.");
+}
+else if (daysUntilExpiration == 1)
+{
+    Console.WriteLine("Your subscription expires within a day!");
+    discountPercentage = 20;
+}
+else if (daysUntilExpiration <= 5)
+{
+    Console.WriteLine($"Your subscription expire in {daysUntilExpiration} days");
+}
+else if (daysUntilExpiration <= 10)
+{
+    Console.WriteLine("Your subscription will expire soon.");
+    discountPercentage = 10;
 }
 
-if (total >= 16)
-{
-    Console.WriteLine("Wagliò hai vinto una macchina!");
-}
-else if (total >= 10)
-{
-    Console.WriteLine("Cuccioloooooo prendi sto portatile.");
-}
-else if (total == 7)
-{
-    Console.WriteLine("Cuoreeee hai vinto una pizza margherita.");
-}
-else 
-{
-    Console.WriteLine("Mi dispiace mister, torni a casa a mani vuote.");
+if (discountPercentage > 0) {
+    Console.WriteLine($"Renew now and save {discountPercentage}%.");
 }
